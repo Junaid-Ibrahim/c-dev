c8295e86d74d441c5a0783f4a14ed3f0af45fd5e
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-02-13 19:13:00 +0530

    refined run() and create()[now edit()]

--- f.sh
+++ f.sh
@@ -1,2 +1,2 @@
-run(){ clang -O0 -Wall -Werror -Wextra $1;shift;./a.out "$@";rm a.out;}
-create(){ cp template.c $1; vis $1;}
+run(){ clang -O0 -Werror -Wall -Wextra $1; shift; ./a.out "$@"; rm -rf ./a.out;}
+edit(){ [ -a $1 ] cp template.c $1; vis $1;}
