1839d23229573d962cdee4a4340e3707763b9320
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-02-22 08:39:40 +0530

    a fault tolerant even/odd seq gen for integers

---
+++ eo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+	int a,b;char d;
+	puts("(e)ven or (o)dd:");
+	scanf("%c",&d);
+	puts("range in the form : start stop");
+	scanf("%d %d",&a,&b);
+	
+	d=(d=='o');
+	if (a%2==0 && d) a+=1;
+	if (b%2==0 && d) b-=1;
+	for (; a<=b; a+=2){ printf("%d ",a);}
+	puts("");
+	return 0;
+}
