a9ccbca12372063c271cbc5dab6c01e3ba41dfb2
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-02-24 21:03:16 +0530

    found out a better way to benchmark

--- eo.c
+++ eo.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//#include<time.h>
 int main(void){
 	int a,b;char d;
 	puts("(e)ven or (o)dd:");
@@ -6,10 +7,13 @@ int main(void){
 	puts("range in the form : start stop");
 	scanf("%d %d",&a,&b);
 	
+	//clock_t e=clock();
 	d=(d=='o');
-	if ((a&1) ^ d) a+=1;
-	if ((b&1) ^ d) b-=1;
+	if ((a%1) ^ d) a+=1;
+	if ((b%1) ^ d) b-=1;
 	for (; a<=b; a+=2){ printf("%d ",a);}
 	puts("");
+	//e=clock()-e;double c=((double)e)/CLOCKS_PER_SEC;
+	//printf("%f\n",c);
 	return 0;
 }
