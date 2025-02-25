9a5f99a0413c25b4ac49550531efb572b4a68517
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-02-25 13:11:28 +0530

    smaller tweaks

--- eo.c
+++ eo.c
@@ -7,13 +7,13 @@ int main(void){
 	scanf("%c",&d);
 	puts("range in the form : start stop");
 	scanf("%d %d",&a,&b);
+	puts("");
 	
 //	clock_t e=clock();
 	d=(d=='o');
 	if ((a&1) ^ d) a+=1;
 	if ((b&1) ^ d) b-=1;
 	for (; a<=b; a+=2){ char * f=int2str(a);puts(f);free(f);}
-	puts("");
 //	e=clock()-e;double c=((double)e)/CLOCKS_PER_SEC;
 //	printf("%f\n",c);
 	return 0;
