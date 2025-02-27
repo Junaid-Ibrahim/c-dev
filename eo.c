cc5ce143ef6746d30a065b458b3b8576215ecfda
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-02-27 22:14:40 +0530

    multiple puts() now replaced by a single puts() by removing int2str()

--- eo.c
+++ eo.c
@@ -11,9 +11,18 @@ int main(void){
 	
 //	clock_t e=clock();
 	d=(d=='o');
-	if ((a&1) ^ d) a+=1;
-	if ((b&1) ^ d) b-=1;
-	for (; a<=b; a+=2){ char * f=int2str(a);puts(f);free(f);}
+	a+=((a&1) ^ d);
+	b-=((b&1) ^ d);
+	int l = b-a;
+	l+=length(a,b)+1;
+	char *h=(char *)malloc((sizeof (char) )*(l+1));
+	int k=l;
+	for (; b>=a; b-=1){
+		int j=b,r;
+		h[k]='\n';
+		while (j!=0){ k-=1;r=j%10;j/=10; h[k]=r+48;}
+		k--;
+	} h[l]='\0';puts(h);
 //	e=clock()-e;double c=((double)e)/CLOCKS_PER_SEC;
 //	printf("%f\n",c);
 	return 0;
