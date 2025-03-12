3ab977fcd1c7bfcfba486795e340300bcbf4b10b
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-03-12 12:29:59 +0530

    new method of input requires new tests

--- test.sh
+++ test.sh
@@ -1,45 +1,20 @@
-clang -O0 eo.c
-printf '\e[32m e \e[0m\n'
-printf '\e[32m 2 6 \e[0m'
-./a.out << f
-e
-2 6
-f
-printf '\e[32m 1 6 \e[0m'
-./a.out << f
-e
-1 6
-f
-printf '\e[32m 1 5 \e[0m'
-./a.out << f
-e
-1 5
-f
-printf '\e[32m 2 5 \e[0m'
-./a.out << f
-e
-2 5
-f
+clang -O3 eo.c
+printf '\e[32m even \e[0m\n'
+printf '\e[32m 2 6 \e[0m\n'
+./a.out e 2 6
+printf '\e[32m 1 6 \e[0m\n'
+./a.out e 1 6
+printf '\e[32m 1 5 \e[0m\n'
+./a.out e 1 5
+printf '\e[32m 2 5 \e[0m\n'
+./a.out e 2 5
 
-printf '\e[32m o \e[0m\n'
-printf '\e[32m 2 6 \e[0m'
-./a.out << f
-o
-2 6
-f
-printf '\e[32m 1 6 \e[0m'
-./a.out << f
-o
-1 6
-f
-printf '\e[32m 1 5 \e[0m'
-./a.out << f
-o
-1 5
-f
-printf '\e[32m 2 5 \e[0m'
-./a.out << f
-o
-2 5
-f
-
+printf '\e[32m odd \e[0m\n'
+printf '\e[32m 2 6 \e[0m\n'
+./a.out o 2 6
+printf '\e[32m 1 6 \e[0m\n'
+./a.out o 1 6
+printf '\e[32m 1 5 \e[0m\n'
+./a.out o 1 5
+printf '\e[32m 2 5 \e[0m\n'
+./a.out o 2 5
