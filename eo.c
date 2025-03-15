b4a6ad38f4fc15b08f3594cfc381a0c4a12c38f5
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-03-15 18:07:05 +0530

    made the eo.c posix compliant

--- eo.c
+++ eo.c
@@ -1,29 +1,26 @@
-#include<stdio.h>
 #include "mylib.h"
-//#include<time.h>
-int main(void){
-	int a,b;char d;
-	puts("(e)ven or (o)dd:");
-	scanf("%c",&d);
-	puts("range in the form : start stop");
-	scanf("%d %d",&a,&b);
-	puts("");
-	
-//	clock_t e=clock();
+int main(int argc, char **argv){
+	int a,b; char d;
+	if (argc==4){
+		d = *argv[1];
+		a = str2int(argv[2]);
+		b = str2int(argv[3]);
+	} else return 1;
+
+	// range adjustment
 	d=(d=='o');
 	a+=((a&1) ^ d);
 	b-=((b&1) ^ d);
-	int l = b-a;
-	l+=length(a,b)+1;
-	if (char *h=(char *)malloc((sizeof (char) )*(l+1)) == NULL) { return -1;}
-	int k=l;
-	for (; b>=a; b-=1){
-		int j=b,r;
-		h[k]='\n';
-		while (j!=0){ k-=1;r=j%10;j/=10; h[k]=r+48;}
-		k--;
-	} h[l]='\0';puts(h);free(h);
-//	e=clock()-e;double c=((double)e)/CLOCKS_PER_SEC;
-//	printf("%f\n",c);
-	return 0;
-}
+
+	//buffer fillup
+	int l = lengt(a,b,1)+length(a,b); // #(spaces) + #(digits)
+	int k=l; char *h; 
+	if((h=(char *)sbrk((sizeof (char) )*(++l))) == ((void *)-1)) { return 2;}
+	for (; b>=a; b-=2){
+		int j=b;
+		h[k--]=040; // 012 is octal for newline and 040 is for space in ascii
+		while (j!=0){ 
+			h[k--]=(j%10)+48; //48 is decimal for zero in ascii
+			j/=10; 
+		}
+	} write(0,h,++l); return 0; }
