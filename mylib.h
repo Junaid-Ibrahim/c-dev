19641662e7008f89f57e853b5a5eac3f394d5cc9
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-03-22 22:47:00 +0530

    making everything long long

--- mylib.h
+++ mylib.h
@@ -1,6 +1,6 @@
 #include<unistd.h>
-int mypow(int a,int b){
-	int i=1;
+long long mypow(long long a,long long b){
+	long long i=1;
 	for(;b>=0;b--){ i*=a;}
 	return i;
 }
@@ -12,30 +12,28 @@ char * int2str(int a){
 	while (b!=0){ r=b%10;b/=10; f[--e]=r+48;}
 	return f;
 }
-int str2int(char* a){
-	int i=0,b=0;
+long long str2int(char* a){
+	long long i=0,b=0;
 	while(a[i]!='\0'){ b=b*10+(a[i++]-48);}
 	return b;
 }
-int max(int n){
-	int s=8;
-	for (--n;n>0;n--){ s+=9*mypow(10,n);}
-	return s;
+long long min(long long n){
+	return mypow(10,n-=2);
 }
-int min(int n){
-	return mypow(10,--n);
+long long max(long long n){
+	return min(n+1)-2;
 }
-int lengt(int a, int b, int c){
+long long lengt(long long a, long long b,long long c){
 	return (((b-a)/2)+1)*c;
 }
-int length(int a , int b){
-	int e=0,f=0,c=a,d=b;
-	while(c!=0){ e+=1;c/=10;}
-	while(d!=0){ f+=1;d/=10;}
-	if (e==f) { return lengt(a,b,e); }
-	int s = lengt(a,max(e),e);
-	s=s+lengt(min(f),b,f);
-	for (int i=e+1;i<f;i++){
+long long length(long long a , long long b){
+	long long da=0,db=0,aa=a,bb=b;
+	while(aa!=0){ da+=1;aa/=10;}
+	while(bb!=0){ db+=1;bb/=10;}
+	if (da==db) { return lengt(a,b,da); }
+	long long s = lengt(a,max(da),da);
+	s=s+lengt(min(db),b,db);
+	for (long long i=da+1;i<db;i++){
 		s=s+5*mypow(10,i-2)+4*mypow(10,i-1);
 	} return s;
 }
