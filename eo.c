ddd2b4b317cd6a0e40f8b26f7263503776deb053
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-03-22 22:48:55 +0530

    ints are now long long and syscall wrappers are now gone

--- eo.c
+++ eo.c
@@ -1,6 +1,6 @@
 #include "mylib.h"
 int main(int argc, char **argv){
-	int a,b; char d;
+	long long a,b; char d;
 	if (argc==4){
 		d = *argv[1];
 		a = str2int(argv[2]);
@@ -13,14 +13,16 @@ int main(int argc, char **argv){
 	b-=((b&1) ^ d);
 
 	//buffer fillup
-	int l = lengt(a,b,1)+length(a,b); // #(spaces) + #(digits)
-	int k=l; char *h; 
-	if((h=(char *)sbrk((sizeof (char) )*(++l))) == ((void *)-1)) { return 2;}
+	long long l = lengt(a,b,1)+length(a,b); // #(spaces) + #(digits)
+	long long k=l; 
+	char *c = (char *)syscall(214,NULL); // 214 is the syscall number for brk
+	syscall(214,c+(sizeof (char)*(l++)));
 	for (; b>=a; b-=2){
-		int j=b;
-		h[k--]=040; // 012 is octal for newline and 040 is for space in ascii
+		long long j=b;
+		c[k--]=040; // 012 is octal for newline and 040 is for space in ascii
 		while (j!=0){ 
-			h[k--]=(j%10)+48; //48 is decimal for zero in ascii
+			c[k--]=(j%10)+48; //48 is decimal for zero in ascii
 			j/=10; 
-		}
-	} write(0,h,++l); return 0; }
+		} c[l-1]=012;
+	} syscall(64,0,c,l); return 0; } // 64 is the syscall number for write
+
