c0a3be752f45226bcb14bd5a019c08a920230fb6
Junaid-Ibrahim <junaidibrahim010@gmail.com>
2025-03-29 18:02:59 +0530

    extensive testing

--- test.sh
+++ test.sh
@@ -1,20 +1,32 @@
-clang -O3 eo.c
-printf '\e[32m even \e[0m\n'
-printf '\e[32m 2 6 \e[0m\n'
+printf '\e[32meven \e[0m\n'
+printf '\e[32m2 6 \e[0m\n'
 ./a.out e 2 6
-printf '\e[32m 1 6 \e[0m\n'
+printf '\e[32m1 6 \e[0m\n'
 ./a.out e 1 6
-printf '\e[32m 1 5 \e[0m\n'
+printf '\e[32m1 5 \e[0m\n'
 ./a.out e 1 5
-printf '\e[32m 2 5 \e[0m\n'
+printf '\e[32m2 5 \e[0m\n'
 ./a.out e 2 5
+printf '\e[32m2 15 \e[0m\n'
+./a.out e 2 15
+printf '\e[32m2 115 \e[0m\n'
+./a.out e 2 115
+printf '\e[32m110 115 \e[0m\n'
+./a.out e 110 115
+echo
 
-printf '\e[32m odd \e[0m\n'
-printf '\e[32m 2 6 \e[0m\n'
+printf '\e[32modd \e[0m\n'
+printf '\e[32m2 6 \e[0m\n'
 ./a.out o 2 6
-printf '\e[32m 1 6 \e[0m\n'
+printf '\e[32m1 6 \e[0m\n'
 ./a.out o 1 6
-printf '\e[32m 1 5 \e[0m\n'
+printf '\e[32m1 5 \e[0m\n'
 ./a.out o 1 5
-printf '\e[32m 2 5 \e[0m\n'
+printf '\e[32m2 5 \e[0m\n'
 ./a.out o 2 5
+printf '\e[32m2 15 \e[0m\n'
+./a.out o 2 15
+printf '\e[32m2 115 \e[0m\n'
+./a.out o 2 115
+printf '\e[32m110 115 \e[0m\n'
+./a.out o 110 115
